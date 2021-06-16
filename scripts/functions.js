43fde1a500cb012201c2c691f44de7b395fba956
Eldan Goldenberg <eg@eldang.xyz>
2021-06-15 17:59:36 -0700

    Moved more functions

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,99 @@
+function setVisibilityState(params) {
+	if ((params.visibleOnLoad === undefined) || (params.visibleOnLoad === false)) {
+		if ((params.legendID !== undefined) && (params.legendID !== false)) {
+			document.getElementById(params.legendID).classList.add('inactive');
+		}
+		return 'none';
+	} else {
+		if ((params.legendID !== undefined) && (params.legendID !== false)) {
+			document.getElementById(params.legendID).classList.remove('inactive');
+		}
+		return 'visible';
+	}
+}
+
+function addPointLayer(map, params) {
+	gus_api(params.gusID, params.gusPage, function(jsondata) {
+		var visibilityState = setVisibilityState(params);
+		if (params.scalingFactor === undefined) { params.scalingFactor = 2.5; }
+		map.addSource(params.sourceName, {
+			type: 'geojson',
+			data: jsondata
+		});
+		map.addLayer({
+			'id': params.layerName,
+			'type': 'symbol',
+			'source': params.sourceName,
+			'layout': {
+				'icon-image': params.icon,
+				'icon-size': params.iconSize,
+				'icon-allow-overlap': true,
+				'visibility': visibilityState
+			}
+		});
+		map.on("zoomend", function(){
+			map.setLayoutProperty(params.layerName, 'icon-size', (1 + (map.getZoom() / originalZoomLevel - 1) * params.scalingFactor) * params.iconSize);
+		});
+		loadedPointLayers.push([params.layerName, params.legendID]);
+		loadedPointLayerNames.push(params.layerName)
+	});
+}
+
+function addVectorLayer(map, params) {
+	var visibilityState = setVisibilityState(params);
+	map.addSource(params.sourceName, {
+		type: 'vector',
+		url: params.sourceURL
+	});
+	if ((params.lineLayerName !== undefined) && (params.lineLayerName !== false)) {
+		map.addLayer(
+			{
+				'id': params.lineLayerName,
+				'type': 'line',
+				'source': params.sourceName,
+				'source-layer': params.sourceID,
+				'layout': {
+					'visibility': visibilityState,
+					'line-join': 'round',
+					'line-cap': 'round'
+				},
+				'paint': {
+					'line-color': params.lineColor,
+					'line-width': 1
+				},
+			},
+			params.displayBehind
+		);
+		if (params.legendID !== undefined) {
+			loadedLineLayers.push([params.lineLayerName, params.legendID]);
+		}
+	}
+	if ((params.polygonLayerName !== undefined) && (params.polygonLayerName !== false)) {
+		if (params.usedInZoomControl) { visibilityState = 'visible'; }
+		map.addLayer(
+			{
+				'id': params.polygonLayerName,
+				'type': 'fill',
+				'source': params.sourceName,
+				'source-layer': params.sourceID,
+				'layout': {
+					'visibility': visibilityState
+				},
+				'paint': {
+					'fill-color': params.polygonFillColor,
+					'fill-outline-color': params.polygonOutlineColor
+				},
+			}
+		);
+		if (params.legendID !== undefined) {
+			loadedPolygonLayers.push([params.polygonLayerName, params.legendID]);
+		}
+	}
+}
+
+
+
+
 // These are the popups for the polygon district layers, using Eldan's House/Senate 'show' logic
 // When a click event occurs on a feature in the unioned districts layer, open a popup for
 // the correct district type at the location of the click, with description HTML from its properties.
